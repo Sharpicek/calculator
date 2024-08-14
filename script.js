30ddcc2b33d74f304288487f96e496c499e7b4a8
Sharpicek <davidthomas13@proton.me>
2024-08-14 21:19:15 +0200

    Create functions add, subtract, multiply, divide and operate

---
+++ script.js
@@ -0,0 +1,36 @@
+let firstNumber, secondNumber, operator;
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function operate(firstNumber, operator, secondNumber) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(firstNumber, secondNumber);
+            break;
+        case "-":
+            result = subtract(firstNumber, secondNumber);
+            break;
+        case "*":
+            result = multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            result =divide(firstNumber, secondNumber);
+            break;
+    }
+    return result;
+};
